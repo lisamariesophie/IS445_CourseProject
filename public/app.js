9a47e0044f3f1cf52804d70543627d22c5e13172
Lisa <lisamariehege@gmail.com>
2019-12-06 01:12:44 -0800

    dev

--- public/app.js
+++ public/app.js
@@ -1,184 +1,111 @@
 
-const createBtn = document.getElementById('createBtn');
-const detailsModal = document.getElementById('detailsModal');
-const db = firebase.firestore();
-const contacts = db.collection('users');
-const tbody = document.getElementById('tbody');
-let counter;
+const createBtn = document.getElementById('createBtn')
+const db = firebase.firestore()
+const contacts = db.collection('contacts')
+let counter
 
-createBtn.addEventListener('click', createContact);
-
+createBtn.addEventListener('click', createContact)
 
 function getContacts() {
+  let table = document.getElementById('tbody');
   contacts.onSnapshot(docs => {
-    tbody.innerHTML = "";
+    table.innerHTML = ""
     counter = 1;
     docs.forEach(doc => {
-      renderContacts(doc)
+      table.innerHTML +=
+        `<tr>
+      <th scope="row">${counter}</th>
+      <td>${doc.data().name}</td>
+      <td>${doc.data().email}</td>
+      <td>${doc.data().phone}</td>
+      <td><button class="btn btn-outline-primary" onclick="showDetails('${doc.id}', '${doc.data().name}', '${doc.data().email}', '${doc.data().phone}')">Details</button></td>
+      <td><button class="btn btn-outline-warning" onclick="editContact('${doc.id}', '${doc.data().name}', '${doc.data().email}', '${doc.data().phone}')">Edit</button></td>
+      <td><button class="btn btn-outline-danger" onclick="deleteContact('${doc.id}')">Delete</button></td>
+      </tr>`
+      counter++
     })
   })
-};
-
-function renderContacts(doc) {
-  let row = document.createElement('tr');
-  row.setAttribute('data-id', doc.id);
-  let tdNumber = document.createElement('td');
-  let tdName = document.createElement('td');
-  let tdEmail = document.createElement('td');
-  let tdPhone = document.createElement('td');
-  let detailsBtn = document.createElement('button');
-  let editBtn = document.createElement('button');
-  let deleteBtn = document.createElement('button');
-  detailsBtn.className = "btn btn-outline-primary";
-  detailsBtn.textContent = "Details";
-  detailsBtn.addEventListener('click', (e) => {
-    e.stopPropagation();
-    let id = e.target.parentElement.getAttribute('data-id');
-    showDetails(id);
-  });
-  editBtn.className = "editBtn btn btn-outline-warning";
-  editBtn.textContent = "Edit";
-  editBtn.addEventListener('click', (e) => {
-    e.stopPropagation();
-    let id = e.target.parentElement.getAttribute('data-id');
-    editContact(id);
-  });
-  deleteBtn.className = "btn btn-outline-danger";
-  deleteBtn.textContent = "Delete";
-  deleteBtn.addEventListener('click', (e) => {
-    e.stopPropagation();
-    let id = e.target.parentElement.getAttribute('data-id');
-    confirmDelete(id);
-  });
-  tdNumber.style.fontWeight = "bold";
-  tdNumber.textContent = counter;
-  tdName.textContent = doc.data().name;
-  tdEmail.textContent = doc.data().email;
-  tdPhone.textContent = doc.data().phone;
-  row.appendChild(tdNumber);
-  row.appendChild(tdName);
-  row.appendChild(tdEmail);
-  row.appendChild(tdPhone);
-  row.appendChild(detailsBtn);
-  row.appendChild(editBtn);
-  row.appendChild(deleteBtn);
-  tbody.appendChild(row);
-  counter++;
-}
-
-async function getContact(id) {
-  const contact = await contacts.doc(id).get().then(function (documentSnapshot) {
-    if (documentSnapshot.exists) {
-      let data = documentSnapshot.data();
-      return data;
-    } else {
-      console.log('document not found');
-    }
-  });
-  return contact;
 }
 
 function createContact() {
-  const form = document.getElementById('createForm');
-  db.collection('users')
+  const form = document.getElementById('createForm')
+  contacts
     .add({
       name: form.name.value,
       email: form.email.value,
       phone: form.phone.value
     })
     .then(function (docRef) {
-      console.log('Document written with ID: ', docRef.id)
-      setId(docRef.id);
-      showDetails(docRef.id);
-      form.name.value = '';
-      form.email.value = '';
-      form.phone.value = '';
+      showDetails(docRef.id, form.name.value, form.email.value, form.phone.value)
+      form.name.value = ''
+      form.email.value = ''
+      form.phone.value = ''
     })
     .catch(function (error) {
       console.error('Error adding document: ', error)
     })
 }
 
-function updateContact(id, contact) {
-  db.collection("users")
-    .doc(id)
-    .update(
-      {
-        name: contact.name,
-        email: contact.email,
-        phone: contact.phone
-      },
-    );
-}
-
-function setId(newId) {
-  db.collection('users').doc(newId).update({
-    id: newId
-  }).then(() => {
-    console.log('ID SET', newId)
-  });
-}
-
-function deleteContact(id) {
-  db.collection("users")
-    .doc(id)
-    .delete()
-    .then(function () {
-      console.log("Document successfully deleted!");
-    }).catch(
-      function (error) {
-        console.error("Error removing document: ", error);
-      });
-}
-
-function confirmDelete(id) {
-  let confirmation = confirm("Are you sure?");
-  if (confirmation == true) {
-    deleteContact(id);
+function editContact(id, name, email, phone) {
+  const updateBtn = document.getElementById('updateBtn')
+  const form = document.getElementById('editForm')
+  $('#editContact').modal('show')
+  form.name.value = name
+  form.email.value = email
+  form.phone.value = phone
+  updateBtn.onclick = function () {
+    $('#editContact').modal('toggle')
+    updateContact(id, form.name.value, form.email.value, form.phone.value)
   }
 }
 
-async function showDetails(id) {
-  $('#detailsContact').modal('show');
-  console.log('DETILS', id);
-  let deleteBtn = document.getElementById('deleteBtn');
-  let editBtn = document.getElementById('editBtn');
-  const contact = await getContact(id);
-  const dName = document.getElementById("dName");
-  const dEmail = document.getElementById("dEmail");
-  const dPhone = document.getElementById("dPhone");
-  dName.innerHTML = `Name: <span>${contact.name}</span>`;
-  dEmail.innerHTML = `Email: <span>${contact.email}</span>`;
-  dPhone.innerHTML = `Phone: <span>${contact.phone}</span>`;
-  deleteBtn.addEventListener('click', (e) => {
-    $('#detailsContact').modal('toggle');
-    e.stopPropagation();
-    confirmDelete(id);
-  });
-  editBtn.addEventListener('click', (e) => {
-    e.stopPropagation();
-    editContact(id);
-  });
+function updateContact(id, name, email, phone) {
+  return contacts.doc(id)
+    .update(
+      {
+        name: name,
+        email: email,
+        phone: phone
+      },
+    ).then(() => {
+      console.log('UPDATED', id)
+    }).catch(function (error) {
+      console.error('Error editing document: ', error)
+    })
 }
 
-async function editContact(id) {
-  console.log(id);
-  $('#editContact').modal('show');
-  const updateBtn = document.getElementById('updateBtn');
-  const form = document.getElementById('editForm');
-  const contact = await getContact(id);
-  form.name.value = contact.name;
-  form.email.value = contact.email;
-  form.phone.value = contact.phone;
-  updateBtn.addEventListener('click', (e) => {
-  $('#editContact').modal('toggle');
-    e.stopPropagation();
-    contact.name = form.name.value;
-    contact.email = form.email.value;
-    contact.phone = form.phone.value;
-    updateContact(id, contact);
-  });
+function deleteContact(id) {
+  let confirmation = confirm("Are you sure?")
+  if (confirmation === true) {
+    contacts
+      .doc(id)
+      .delete()
+      .then(() => {
+        console.log("DELETED", id)
+      }).catch(
+        function (error) {
+          console.error("Error removing document: ", error)
+        })
+  }
 }
 
+function showDetails(id, name, email, phone) {
+  const editBtn = document.getElementById("editBtn")
+  $('#detailsContact').modal('show')
+  const dName = document.getElementById("dName")
+  const dEmail = document.getElementById("dEmail")
+  const dPhone = document.getElementById("dPhone")
+  dName.innerHTML = `Name: <span>${name}</span>`
+  dEmail.innerHTML = `Email: <span>${email}</span>`
+  dPhone.innerHTML = `Phone: <span>${phone}</span>`
+  deleteBtn.onclick = function () {
+    $('#detailsContact').modal('toggle')
+    deleteContact(id)
+  }
+  editBtn.onclick = function () {
+    $('#detailsContact').modal('toggle')
+    editContact(id, name, email, phone);
+  }
+}
 
-getContacts();
+getContacts()
