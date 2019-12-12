3a9c6a8fe97163a75dbeb3e849894be0cce26d34
Lisa <lisamariehege@gmail.com>
2019-12-11 23:11:33 -0800

    forms

--- public/app.js
+++ public/app.js
@@ -1,17 +1,17 @@
-const createBtn = document.getElementById('createBtn')
-const updateBtn = document.getElementById('updateBtn')
-const db = firebase.firestore()
-const contacts = db.collection('contacts')
-let counter
+const createBtn = document.getElementById('createBtn');
+const updateBtn = document.getElementById('updateBtn');
+const db = firebase.firestore();
+const contacts = db.collection('contacts');
+let counter;
 
-createBtn.addEventListener('click', createContact)
+createBtn.addEventListener('click', createContact);
 
 //get Contacts from Firebase and render Table
 function getContacts() {
-  const table = document.getElementById('tbody')
+  const table = document.getElementById('tbody');
   contacts.onSnapshot(docs => {
-    table.innerHTML = ""
-    counter = 1
+    table.innerHTML = "";
+    counter = 1;
     docs.forEach(doc => {
       table.innerHTML +=
         `<tr>
@@ -22,15 +22,15 @@ function getContacts() {
       <td><button class="btn btn-outline-primary" onclick="showDetails('${doc.id}', '${doc.data().name}', '${doc.data().email}', '${doc.data().phone}')">Details</button></td>
       <td><button class="btn btn-outline-warning" onclick="editContact('${doc.id}', '${doc.data().name}', '${doc.data().email}', '${doc.data().phone}')">Edit</button></td>
       <td><button class="btn btn-outline-danger" onclick="deleteContact('${doc.id}')">Delete</button></td>
-      </tr>`
-      counter++
-    })
-  })
+      </tr>`;
+      counter++;
+    });
+  });
 }
 
 //create new Contact in Firebase
 function createContact() {
-  const createForm = document.getElementById('createForm')
+  const createForm = document.getElementById('createForm');
   contacts
     .add({
       name: createForm.name.value,
@@ -38,27 +38,27 @@ function createContact() {
       phone: createForm.phone.value
     })
     .then(function (docRef) {
-      showDetails(docRef.id, createForm.name.value, createForm.email.value, createForm.phone.value)
-      createForm.name.value = ''
-      createForm.email.value = ''
-      createForm.phone.value = ''
+      showDetails(docRef.id, createForm.name.value, createForm.email.value, createForm.phone.value);
+      createForm.name.value = '';
+      createForm.email.value = '';
+      createForm.phone.value = '';
     })
     .catch(function (error) {
-      console.error('Error adding document: ', error)
-    })
+      console.error('Error adding document: ', error);
+    });
 }
 
 //edit Contact
 function editContact(id, name, email, phone) {
-  const editForm = document.getElementById('editForm')
-  $('#editContact').modal('show')
-  editForm.name.value = name
-  editForm.email.value = email
-  editForm.phone.value = phone
+  const editForm = document.getElementById('editForm');
+  $('#editContact').modal('show');
+  editForm.name.value = name;
+  editForm.email.value = email;
+  editForm.phone.value = phone;
   updateBtn.onclick = function () {
-    $('#editContact').modal('toggle')
-    updateContact(id, editForm.name.value, editForm.email.value, editForm.phone.value)
-  }
+    $('#editContact').modal('toggle');
+    updateContact(id, editForm.name.value, editForm.email.value, editForm.phone.value);
+  };
 }
 
 //update Contact in Firebase
@@ -69,48 +69,48 @@ function updateContact(id, name, email, phone) {
         name: name,
         email: email,
         phone: phone
-      },
+      }
     ).then(() => {
-      console.log('UPDATED', id)
+      console.log('UPDATED', id);
     }).catch(function (error) {
-      console.error('Error editing document: ', error)
-    })
+      console.error('Error editing document: ', error);
+    });
 }
 
 //delete Contact from Firebase
 function deleteContact(id) {
-  let confirmation = confirm("Are you sure?")
+  let confirmation = confirm("Are you sure?");
   if (confirmation === true) {
     contacts
       .doc(id)
       .delete()
       .then(() => {
-        console.log("DELETED", id)
+        console.log("DELETED", id);
       }).catch(
         function (error) {
-          console.error("Error removing document: ", error)
-        })
+          console.error("Error removing document: ", error);
+        });
   }
 }
 
 //show Details and Options in Modal
 function showDetails(id, name, email, phone) {
-  const editBtn = document.getElementById("editBtn")
-  $('#detailsContact').modal('show')
-  const dName = document.getElementById("dName")
-  const dEmail = document.getElementById("dEmail")
-  const dPhone = document.getElementById("dPhone")
-  dName.innerHTML = `Name: <span>${name}</span>`
-  dEmail.innerHTML = `Email: <span>${email}</span>`
-  dPhone.innerHTML = `Phone: <span>${phone}</span>`
+  const editBtn = document.getElementById("editBtn");
+  $('#detailsContact').modal('show');
+  const dName = document.getElementById("dName");
+  const dEmail = document.getElementById("dEmail");
+  const dPhone = document.getElementById("dPhone");
+  dName.innerHTML = `Name: <span>${name}</span>`;
+  dEmail.innerHTML = `Email: <span>${email}</span>`;
+  dPhone.innerHTML = `Phone: <span>${phone}</span>`;
   deleteBtn.onclick = function () {
-    $('#detailsContact').modal('toggle')
-    deleteContact(id)
-  }
+    $('#detailsContact').modal('toggle');
+    deleteContact(id);
+  };
   editBtn.onclick = function () {
-    $('#detailsContact').modal('toggle')
+    $('#detailsContact').modal('toggle');
     editContact(id, name, email, phone);
-  }
+  };
 }
 
-getContacts()
+getContacts();
--- public/formValidation.js
+++ public/formValidation.js
@@ -1,64 +1,63 @@
-const createName = document.getElementById('createName')
-const createEmail = document.getElementById('createEmail')
-const createPhone = document.getElementById('createPhone')
-const editName = document.getElementById('editName')
-const editEmail = document.getElementById('editEmail')
-const editPhone = document.getElementById('editPhone')
-let isValidCreateEmail
-let isValidCreateName
-let isValidCreatePhone
-let isValidEditEmail = true
-let isValidEditName = true
-let isValidEditPhone = true
-
+const createName = document.getElementById('createName');
+const createEmail = document.getElementById('createEmail');
+const createPhone = document.getElementById('createPhone');
+const editName = document.getElementById('editName');
+const editEmail = document.getElementById('editEmail');
+const editPhone = document.getElementById('editPhone');
+let isValidCreateEmail;
+let isValidCreateName;
+let isValidCreatePhone;
+let isValidEditEmail = true;
+let isValidEditName = true;
+let isValidEditPhone = true;
 
 //Input Validation for CreateContacts Form
 createName.addEventListener('keyup', () => {
-    isValidCreateName = createName.checkValidity()
-    createInputsValid()
-})
+    isValidCreateName = createName.checkValidity();
+    createInputsValid();
+});
 
 createEmail.addEventListener('keyup', () => {
-    isValidCreateEmail = createEmail.checkValidity()
-    createInputsValid()
-})
+    isValidCreateEmail = createEmail.checkValidity();
+    createInputsValid();
+});
 
 createPhone.addEventListener('keyup', () => {
-    isValidCreatePhone = createPhone.checkValidity()
-    createInputsValid()
-})
+    isValidCreatePhone = createPhone.checkValidity();
+    createInputsValid();
+});
 
 function createInputsValid() {
     if (isValidCreateName && isValidCreateEmail && isValidCreatePhone) {
-        createBtn.disabled = false
+        createBtn.disabled = false;
     }
     else {
-        createBtn.disabled = true
+        createBtn.disabled = true;
     }
 }
-
 
 //Input Validation for EditContacts Form
 editName.addEventListener('keyup', () => {
-    isValidEditName = editName.checkValidity()
-    editInputsValid()
-})
+    isValidEditName = editName.checkValidity();
+    editInputsValid();
+});
 
 editEmail.addEventListener('keyup', () => {
-    isValidEditEmail = editEmail.checkValidity()
-    editInputsValid()
-})
+    isValidEditEmail = editEmail.checkValidity();
+    editInputsValid();
+});
 
 editPhone.addEventListener('keyup', () => {
-    isValidEditPhone = editPhone.checkValidity()
-    editInputsValid()
-})
+    isValidEditPhone = editPhone.checkValidity();
+    editInputsValid();
+});
 
 function editInputsValid() {
     if (isValidEditName && isValidEditEmail && isValidEditPhone) {
-        updateBtn.disabled = false
+        updateBtn.disabled = false;
     }
     else {
-        updateBtn.disabled = true
+        updateBtn.disabled = true;
     }
 }
+
